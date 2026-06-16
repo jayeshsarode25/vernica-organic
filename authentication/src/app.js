3da95a5b7aadc6e6b8906250cd588a1975ba7bf8
jayeshsarode25 <jayeshsarode2003@gmail.com>
2026-06-16 15:37:29 +0530

    fix auth startup on hostinger

--- authentication/src/app.js
+++ authentication/src/app.js
@@ -11,6 +11,7 @@ import { applySecurityMiddleware } from './middleware/Security.middleware.js'
 
 
 const app = express();
+app.set("trust proxy", 1);
 
 const allowedOrigins = (process.env.FRONTEND_URL || "http://localhost:5173")
   .split(",")
