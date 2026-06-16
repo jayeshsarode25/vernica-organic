198fff88c20035320940b3e8f9ce55c826a72323
jayeshsarode25 <jayeshsarode2003@gmail.com>
2026-06-16 12:31:06 +0530

    fix hostinger auth deployment

--- authentication/server.js
+++ authentication/server.js
@@ -1,17 +1,17 @@
 import _config from './src/config/config.js';
 import app from './src/app.js';
 import connectDB from './src/db/db.js';
-import { connectRedis } from './src/db/redis.js';
-
+// import { connectRedis } from './src/db/redis.js';
 
 await connectDB();
-await connectRedis();
-
-
-
+// await connectRedis();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=>{
-    console.log(`Authentication server running on port ${PORT}`);
+app.get("/", (req, res) => {
+  res.send("Authentication Service Running");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Authentication server running on port ${PORT}`);
 });
